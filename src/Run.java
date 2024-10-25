7629feb99dcfc85f2761de7f361a3d1e43cb95dd
BT_1N3_17 <sample@bths.edu>
2024-10-25 10:09:57 -0400

    3

--- src/Run.java
+++ src/Run.java
@@ -2,5 +2,7 @@ public class Run {
     public static void main(String[] args) {
         System.out.println("Test");
         // blah
+        String str = "test";
+        System.out.println(str);
     }
 }
